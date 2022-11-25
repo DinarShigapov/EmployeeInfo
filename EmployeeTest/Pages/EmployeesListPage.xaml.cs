5f2d5b17e78cfc912e4d9a55f0caf6473f716ed5
DinarShigapov <dinarhertz@gmail.com>
2022-11-25 21:04:41 +0300

    Добавьте файлы проекта.

---
+++ EmployeeTest/Pages/EmployeesListPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeeTest.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeesListPage.xaml
+    /// </summary>
+    public partial class EmployeesListPage : Page
+    {
+        public EmployeesListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
